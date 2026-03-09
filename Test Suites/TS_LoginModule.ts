7bc040bd6c325a9142af9fb6633a2f72598f7b6b
Keda1021 <cbtkiet1610@gmail.com>
2026-03-10 00:12:50 +0700

    create blank customerID

---
+++ Test Suites/TS_LoginModule.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_LoginModule</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3dce2061-601d-4b8b-a104-8aafdd3a3bea</testSuiteGuid>
+   <testCaseLink>
+      <guid>fcfda551-44e2-4422-b571-9dfdc0699d9a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Manager Module/TC01_Login_Success</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
