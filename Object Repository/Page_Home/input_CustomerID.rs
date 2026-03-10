76d48079d52f9216b7b4dccf22e3763b3a04843c
Keda1021 <cbtkiet1610@gmail.com>
2026-03-10 14:09:10 +0700

    New test object

---
+++ Object Repository/Page_Home/input_CustomerID.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_CustomerID</name>
+   <tag></tag>
+   <elementGuidId>c2b8861b-bde2-4bdb-9901-738d406de2e3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='cusid']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
